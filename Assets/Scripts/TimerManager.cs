37970eda5d31f7fb08a02039d5e908fa38e4d67b
lganic <55366941+lganic@users.noreply.github.com>
2026-03-01 12:19:53 -0500

    Add countdown timer

---
+++ Assets/Scripts/TimerManager.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TimerManager : MonoBehaviour
+{
+    public Text TimerText;
+    public Text CountdownText;
+
+    public float TimerAmount = 0;
+    private float TimerBase;
+    private AudioSource ass;
+
+    private float TimerStart;
+
+    public int leadup = 5;
+
+    public string TargetEndScene;
+
+    void StartTimer(float length)
+    {
+        TimerAmount = length;
+        TimerStart = Time.time;
+        ass.Play();
+    }
+
+    private void Start()
+    {
+        ass = gameObject.GetComponent<AudioSource>();
+        StartTimer(20);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (TimerAmount > 0)
+        {
+            float elapsed_time = Time.time - TimerStart;
+
+            if (elapsed_time < leadup + 1)
+            {
+                string cd = ((int)(leadup - elapsed_time)).ToString();
+
+                if (cd == "0")
+                {
+                    cd = "Go!";
+                }
+
+                CountdownText.text = cd;
+            }
+
+            else
+            {
+                CountdownText.text = "";
+
+                elapsed_time = TimerAmount - (elapsed_time - leadup - 1);
+
+                if (elapsed_time < 0)
+                {
+                    SceneManager.LoadScene(TargetEndScene);
+                }
+
+                string p1 = ((int)elapsed_time / 60).ToString();
+                string p2 = (((int)elapsed_time) % 60).ToString();
+
+                if (p2.Length == 1)
+                {
+                    p2 = "0" + p2;
+                }
+
+                TimerText.text = p1 + ":" + p2;
+            }
+
+        }
+        
+    }
+}
